9be82e428db4cc504602f27027531042fb3f4c66
xeropine <susanshrestha783@gmail.com>
2021-04-10 23:13:35 +0545

    updated password reset

--- app/src/main/java/com/example/passthenote/Login.java
+++ app/src/main/java/com/example/passthenote/Login.java
@@ -1,10 +1,13 @@
 package com.example.passthenote;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,18 +17,26 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
-    Button registerButtonInLogin, loginButton;
+    Button registerButtonInLogin, loginButton, resetPassBtn;
     EditText userEmail, password;
     FirebaseAuth firebaseAuth;
+    //FirebaseUser user;
+    AlertDialog.Builder reset_alert;
+    LayoutInflater inflater;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         firebaseAuth = FirebaseAuth.getInstance();
-
+        //user = firebaseAuth.getCurrentUser();
+        reset_alert = new AlertDialog.Builder(this);
+        inflater = this.getLayoutInflater();
+        resetPassBtn = findViewById(R.id.resetPassBtn);
         registerButtonInLogin = findViewById(R.id.registerButtonInLogin);
         registerButtonInLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +53,7 @@ public class Login extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //extract data and validate
+
                 if(userEmail.getText().toString().isEmpty()){
                     userEmail.setError("E-mail is missing");
                     return;
@@ -61,8 +73,47 @@ public class Login extends AppCompatActivity {
                     @Override
                     public void onFailure(@NonNull Exception e) {
                         Toast.makeText(Login.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        resetPassBtn.setVisibility(View.VISIBLE);
                     }
                 });
+            }
+        });
+
+        resetPassBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //start alertdiving
+                View view = inflater.inflate(R.layout.reset_pop,null);
+                reset_alert.setTitle("Reset Password?")
+                        .setMessage("Enter your e-mail address to reset the password")
+                        .setPositiveButton("Reset", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                // validate the e-mail address
+                                EditText email = view.findViewById(R.id.resetPopEmail);
+                                if(email.getText().toString().isEmpty()){
+                                    email.setError("Required Field");
+                                    return;
+                                }
+
+                                // send the reset link
+                                firebaseAuth.sendPasswordResetEmail(email.getText().toString()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Toast.makeText(Login.this, "Reset e-mail sent", Toast.LENGTH_SHORT).show();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(Login.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                                    }
+                                });
+
+                            }
+                        }).setNegativeButton("Cancel", null)
+                        .setView(view)
+                        .create().show();
             }
         });
 
--- app/src/main/java/com/example/passthenote/ResetPassword.java
+++ app/src/main/java/com/example/passthenote/ResetPassword.java
@@ -12,13 +12,16 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class ResetPassword extends AppCompatActivity {
     EditText userPassword, userConfPassword;
     Button resetPasswordBtn;
+    EditText password;
     FirebaseUser user;
+    FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,31 +31,45 @@ public class ResetPassword extends AppCompatActivity {
         userConfPassword = findViewById(R.id.confirmNewPassword);
         user = FirebaseAuth.getInstance().getCurrentUser();
         resetPasswordBtn = findViewById(R.id.resetPasswordBtn);
+        password  = findViewById(R.id.currentPasswordReset);
+        firebaseAuth = FirebaseAuth.getInstance();
 
         resetPasswordBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(userPassword.getText().toString().isEmpty()){
-                    userPassword.setError("Not filled");
-                    return;
-                } if(userConfPassword.getText().toString().isEmpty()){
-                    userConfPassword.setError("Not filled");
-                    return;
-                } if(!userConfPassword.getText().toString().equals(userPassword.getText().toString())){
-                    userConfPassword.setError("Passwords do not match");
-                    return;
-                }
 
-                user.updatePassword(userConfPassword.getText().toString()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                // checks for the confirmation of current password of the user
+                String email = firebaseAuth.getCurrentUser().getEmail();
+                firebaseAuth.signInWithEmailAndPassword(email, password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                     @Override
-                    public void onSuccess(Void aVoid) {
-                        Toast.makeText(ResetPassword.this, "Password successfully changed", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    public void onSuccess(AuthResult authResult) {
+                        if(userPassword.getText().toString().isEmpty()){
+                            userPassword.setError("Not filled");
+                            return;
+                        } if(userConfPassword.getText().toString().isEmpty()){
+                            userConfPassword.setError("Not filled");
+                            return;
+                        } if(!userConfPassword.getText().toString().equals(userPassword.getText().toString())){
+                            userConfPassword.setError("Passwords do not match");
+                            return;
+                        }
+                        user.updatePassword(userConfPassword.getText().toString()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(ResetPassword.this, "Password successfully changed", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(ResetPassword.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
                     }
                 }).addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(ResetPassword.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(ResetPassword.this, "Current Password Incorrect", Toast.LENGTH_LONG).show();
                     }
                 });
             }
